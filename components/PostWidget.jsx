77aa069117cac271228cb011eb4241801a8f1bf2
Theo L <atlufkin@outlook.com>
2022-05-31 17:11:57 -0400

    “updated”

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+import Link from 'next/link';
 
-const PostWidget = () => {
+import { getRecentPosts, getSimilarPosts } from '../services';
+
+const PostWidget = ({ categories, slug }) => {
+    const [relatedPosts, setRelatedPosts] = useState([]);
+
+    useEffect(() => {
+        if (slug) {
+            getSimilarPosts(categories, slug)
+                .then((result) => setRelatedPosts(result))
+        } else {
+            getRecentPosts()
+                .then((result) => setRelatedPosts(result))
+        }
+    }, [slug])
+
+    console.log(relatedPosts);
+
     return (
-        <div>PostWidget</div>
+        <div className='bg-white shadow-lg rounded-lg p-8 mb-8'>
+            <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
+                {slug ? 'Related Posts' : 'React Posts'}
+            </h3>
+            {relatedPosts.map((post) => (
+                <div key={post.title} className='flex items-center w-full mb-4'>
+                    <div className='w-16 flex-none'>
+                        <img
+                            alt={post.title}
+                            height="60px"
+                            width="60px"
+                            className='align-middle rounded-full'
+                            src={post.featuredImage.url} />
+                    </div>
+                    <div className='flex-grow ml-4'>
+                        <p>
+                            {moment(post.createdAt).format('MMM DD, YYYY')}
+                        </p>
+                    </div>
+                </div>
+            ))}
+        </div>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css';
-
-const posts = [
-  { title: 'Learning Next.js!', excerpt: ' learn how to use NExt and GraphQl' },
-  { title: 'Learning Tailwindcss!', excerpt: ' learn how to use Next and Tailwind styling' }
-]
+import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services';
 
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className='container mx-auto px-10 mb-8'>
       <Head>
@@ -17,19 +14,23 @@ export default function Home() {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post, index) => (
-            <div>
-              {post.title}
-              {post.excerpt}
-            </div>
-          ))}
+          {posts.map((post, index) => (<PostCard post={post.node} key={post.title} />))}
         </div>
         <div className='lg:col-span-4 col-span-1'>
           <div className='lg:sticky relative top-8'>
-
+            <PostWidget />
+            <Categories />
           </div>
         </div>
       </div>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts }
+  }
 }
