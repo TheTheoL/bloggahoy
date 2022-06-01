e83fb692eb5398c6072e9ececd9e5cde0e827a81
Theo L <atlufkin@outlook.com>
2022-06-01 16:35:05 -0400

    “updated”

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -35,9 +35,12 @@ const PostWidget = ({ categories, slug }) => {
                             src={post.featuredImage.url} />
                     </div>
                     <div className='flex-grow ml-4'>
-                        <p>
+                        <p className='text-gray-500 font-xs'>
                             {moment(post.createdAt).format('MMM DD, YYYY')}
                         </p>
+                        <Link href={`/post/${post.slug}`} key={post.title} className='text-md'>
+                            {post.title}
+                        </Link>
                     </div>
                 </div>
             ))}
