f0b88c9a078f5c7e60afd37c3a1ce515bcbc1070
Theo L <atlufkin@outlook.com>
2022-06-08 13:24:10 -0400

    “updated”

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -21,9 +21,9 @@ export default async function comments(req, res) {
 
   const query = gql`
     mutation CreateComment($name: String!, $email: String!, $comment: String!, $slug: String!) {
-      createComment(data: { name: $name, email: $email, comment: $comment, post: { connect: { slug: $slug } }) { id }
+      createComment(data: {name: $name, email: $email, comment: $comment, post: {connect: {slug: $slug}}}) { id }
     }
-  `
+  `;
   try {
     const result = await graphQLClient.request(query, req.body)
     return res.status(200).send(result);
